8f2d88802d544ab6e3a3f5695c212140ffc3ed6b
Shant H <shanth2600@gmail.com>
2015-09-15 12:06:54 -0700

    creating master blade template and login template

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,32 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+         <meta charset="UTF-8"> 
+        <title>Home</title> 
+        <!-- Bootstrap core CSS -->
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+        <link href="assets/css/main.css" rel="stylesheet">
+        <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    </head>
+<body>
+	@yield('content')
+</body>
+<footer class="container-fluid" id="footer">
+	<div class="footer col-lg-12">
+	    <div class="col-lg-4">
+	    <ul>
+	        <li><a href="index.php">Home</a></li>
+	    </ul>
+	    </div>
+	</div>
+</footer>    
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.11.3/jquery-ui.min.js"></script>
+
+    <script src="assets/js/bootstrap.min.js"></script>    
+    </body>
+</html>
